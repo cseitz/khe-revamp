d26c6bf00f123e26f753cfdfa9f09afb759ae0a2
cseitz5 <cseitz5@kent.edu>
2020-06-24 15:12:01 -0400

    idk

--- backend/DEV_NOTES.md
+++ backend/DEV_NOTES.md
@@ -7,3 +7,24 @@
 - [X] Some sick tunes to code to
 
 bla bla bla, add more stuff here.
+
+
+
+
+1. Email, Pass, Confirm Pass
+2. Full Name
+3. School/University
+4. Phone
+5. Shirt Size
+6. First Hackathon (Y/N)
+7. Dietary Restrictions (Vegetarian, Vegan, Kosher, Gluten Free, OTHER)
+8. Level of schooling (high school, freshman, sophomore, etc)
+9. Age
+10. Gender
+11. Major
+12. Travel Reimbursment
+13. Upload Resume
+14. Personal Website
+15. Agree to MLH conduct
+16. Send MLH data
+17. FINISHED
---
+++ backend/config.js
@@ -0,0 +1,18 @@
+let path = require("path");
+
+exports.port = 3010;
+
+exports.dist = path.join(__dirname, "../frontend/dist");
+
+exports.staff_oauth = {
+  "github": {
+    redirect: "https://github.com/login/oauth/access_token?client_id={{clientID}}&client_secret=${clientSecret}&code={{requestToken}}",
+
+  },
+
+}
+
+
+// Test Credentials that aren't uploaded to github for security reasons
+if (require("fs").existsSync(__dirname + "/config.test.js"))
+  require(__dirname + "/config.test.js")(module.exports);
